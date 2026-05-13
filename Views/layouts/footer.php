30068e224065e881cfffcaf163b7bf0f97bbbd75
tinhpv10 <117355971+tinhpv10@users.noreply.github.com>
2026-05-13 13:46:23 +0700

    [Giao diện] - Footer  cơ bản đã dịch

--- Views/layouts/footer.php
+++ Views/layouts/footer.php
@@ -1,6 +1,59 @@
- <h6>Cuối trang</h6>
+<footer class="footer bg-dark text-white py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="logo">
+                    <img height="150" src="https://inkythuatso.com/uploads/thumbnails/800/2021/12/logo-fpt-polytechnic-inkythuatso-09-13-08-21.jpg" alt="">
+                </div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, est! Molestias dolore praesentium, eius doloremque quaerat veniam, commodi corrupti vitae cumque animi assumenda odit consectetur aliquid quae nostrum mollitia accusantium!
+            </div>
+            <div class="col-lg-3">
+                <h5 class="title-footer">
+                    Dịch vụ
+                </h5>
+                <div class="list-group ">
+                    <a href="#" class="list-group-item bg-dark text-white  list-group-item-action" aria-current="true">
+                        The current link item
+                    </a>
+                    <a href="#" class="list-group-item bg-dark text-white  list-group-item-action">A second link item</a>
+                    <a href="#" class="list-group-item bg-dark text-white  list-group-item-action">A third link item</a>
+                    <a href="#" class="list-group-item bg-dark text-white  list-group-item-action">A fourth link item</a>
+                    <a href="#" class="list-group-item bg-dark text-white  list-group-item-action disabled" aria-disabled="true">A disabled link item</a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                 <h5 class="title-footer">
+                    Khoá học
+                </h5>
+                <div class="list-group">
+                    <a href="#" class="list-group-item bg-dark text-white list-group-item-action" aria-current="true">
+                        The current link item
+                    </a>
+                    <a href="#" class="list-group-item bg-dark text-white list-group-item-action">A second link item</a>
+                    <a href="#" class="list-group-item bg-dark text-white list-group-item-action">A third link item</a>
+                    <a href="#" class="list-group-item bg-dark text-white list-group-item-action">A fourth link item</a>
+                    <a href="#" class="list-group-item bg-dark text-white list-group-item-action disabled" aria-disabled="true">A disabled link item</a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                 <h5 class="title-footer">
+                    Chính sách
+                </h5>
+                <div class="list-group">
+                    <a href="#" class="list-group-item bg-dark text-white list-group-item-action" aria-current="true">
+                        The current link item
+                    </a>
+                    <a href="#" class="list-group-item bg-dark text-white list-group-item-action">A second link item</a>
+                    <a href="#" class="list-group-item bg-dark text-white list-group-item-action">A third link item</a>
+                    <a href="#" class="list-group-item bg-dark text-white list-group-item-action">A fourth link item</a>
+                    <a href="#" class="list-group-item bg-dark text-white list-group-item-action disabled" aria-disabled="true">A disabled link item</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 
 </html>
